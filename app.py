64ea56584f719f61258dfea59daae60af68d6efd
bimacias <bimacias14@gmail.com>
2022-06-08 20:28:15 +0000

    added more functionality

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
 import flask
-from flask import Flask, abort, redirect, request, render_template, session, url_for
+from flask import Flask, abort, make_response, redirect, request, render_template, session, url_for
 from oauthlib.oauth2 import WebApplicationClient
-from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
-import cachecontrol
-import google.auth.transport.requests
+from googleapiclient.discovery import build
 import json
 import os
-import requests
 
 
-# gets client_id, client_secret, and applicable uris from client_secrets json file downloaded from gcp
+# gets CLIENT_ID, CLIENT_SECRET, and applicable uris from client_secrets json file downloaded from api console
 def get_all():
     filename = "client_secrets.json"  # TODO: change filename/path to wherever secrets json file is located
     with open(filename) as file:
         data_dict = json.load(file)
-    return data_dict['web']['client_id'], data_dict['web']['client_secret'], data_dict['web']['auth_uri'], \
-           data_dict['web']['redirect_uris'][0]
+    return data_dict['web']['client_id'], data_dict['web']['client_secret'], data_dict['web']['redirect_uris'][0]
 
 
 # set up env vars
@@ -27,15 +23,15 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 app = Flask(__name__)
 
 # set up OAuth2.0 client
-discovery_url = ""
-client_id, client_secret, auth_uri, redirect_uri = get_all()
-client = WebApplicationClient(client_id)
+CLIENT_ID, CLIENT_SECRET, REDIRECT_URI = get_all()
+CLIENT = WebApplicationClient(CLIENT_ID)
 
 # obtain user info and use scopes to identify app and specify scopes for requesting authorization
-scps = []
+# scopes can be changed to any applicable scopes that the app wants access to
+SCOPES = []
 flow = Flow.from_client_secrets_file(
-    'client_secrets.json',
-    scopes=scps)
+    "client_secrets.json",
+    scopes=SCOPES)
 
 
 def login_required(function):
@@ -55,55 +51,65 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    flow.redirect_uri = redirect_uri
+    flow.redirect_uri = REDIRECT_URI
     auth_url, state = flow.authorization_url(access_type="offline", prompt="consent", include_granted_scopeds="true")
-    print(auth_url)
+    session['state'] = state
     return redirect(auth_url)
 
 
 @app.route('/callback', methods=['GET', 'POST'])
 def callback():
-    curr_state = session['state']
     flow.redirect_uri = url_for("oauth2callback", _external=True)
 
     auth_response = request.url
     flow.fetch_token(authorization_response=auth_response)
 
+    # verify state received matches session token
     if session['state'] != request.args['state']:
-        abort(500)
+        res = make_response(json.dumps('Invalid state'), 401)
+        res.headers['Content-Type'] = "application/json"
+        return res
 
     creds = flow.credentials
     session['credentials'] = {
         'token': creds.token,
         'refresh_token': creds.refresh_token,
         'token_uri': creds.token_uri,
-        'client_id': creds.client_id,
+        'CLIENT_ID': creds.client_id,
         'client_secret': creds.client_secret,
         'scopes': creds.scopes
     }
-    # flow.fetch_token(authorization_response=request.url)
-    # if not session["state"] == request.args["state"]:
-    #     abort(500)
-    #
-    # credentials = flow.credentials
-    # req_session = requests.session()
-    # cached_session = cachecontrol.CacheControl(req_session)
-    # token_req = google.auth.transport.requests.Request(session=cached_session)
-    #
-    # info = id_token.verify_oauth2_token(id_token=credentials.id_token, request=token_req, audience=client_id)
-    #
-    # session["google_id"] = info.get("sub")
-    # session["name"] = info.get("name")
-    # return redirect("/in")
+
+    return redirect(url_for('test'))
 
 
-@app.route("/in", methods=['GET', 'POST'])
+@app.route('/test', methods=['GET', 'POST'])
 @login_required
-def logged_in():
-    return render_template("home.html", name=session["name"])
+def test_login():
+    if "credentials" not in session:
+        return redirect('login')
+
+    creds = google.oauth2.credentials.Credentials(**session['credentials'])
+    profile =
+
+    session['credentials'] = {
+        'token': creds.token,
+        'refresh_token': creds.refresh_token,
+        'token_uri': creds.token_uri,
+        'CLIENT_ID': creds.client_id,
+        'client_secret': creds.client_secret,
+        'scopes': creds.scopes
+    }
+
+    creds = session['credentials']
+
+    serv = build("oauth2", "", credentials=creds)
+    user = serv.userinfo().get().execute()
+
+    return render_template("home.html", name=)
 
 
-@app.route("/logout", methods=['GET', 'POST'])
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.clear()
     return redirect("/")
